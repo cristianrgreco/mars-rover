e9b3905b0dec16f8fb94ef71eec06192e58b3972
Cristian Greco <cristianrgreco@gmail.com>
2018-08-25 10:06:04 +0100

    Impove test

--- src/location.test.js
+++ src/location.test.js
@@ -1,5 +1,5 @@
 const nextLocation = require("./location");
-const { NORTH } = require("./constants");
+const { NORTH, SOUTH, EAST, WEST } = require("./constants");
 
 describe("nextLocation", () => {
   it("should return the next location", () => {
@@ -10,11 +10,38 @@ describe("nextLocation", () => {
     expect(nextLocation(direction, grid, location)).toEqual({ x: 0, y: 1 });
   });
 
-  it("should return the next location without leaving the grid", () => {
-    const direction = NORTH;
-    const grid = { x: 1, y: 1 };
-    const location = { x: 0, y: 1 };
+  it("should not move outside the grid", () => {
+    const testCases = [
+      () => {
+        const direction = NORTH;
+        const grid = { x: 1, y: 1 };
+        const location = { x: 0, y: 1 };
 
-    expect(nextLocation(direction, grid, location)).toEqual({ x: 0, y: 1 });
+        expect(nextLocation(direction, grid, location)).toEqual({ x: 0, y: 1 });
+      },
+      () => {
+        const direction = SOUTH;
+        const grid = { x: 1, y: 1 };
+        const location = { x: 0, y: 0 };
+
+        expect(nextLocation(direction, grid, location)).toEqual({ x: 0, y: 0 });
+      },
+      () => {
+        const direction = EAST;
+        const grid = { x: 1, y: 1 };
+        const location = { x: 1, y: 0 };
+
+        expect(nextLocation(direction, grid, location)).toEqual({ x: 1, y: 0 });
+      },
+      () => {
+        const direction = WEST;
+        const grid = { x: 1, y: 1 };
+        const location = { x: 0, y: 0 };
+
+        expect(nextLocation(direction, grid, location)).toEqual({ x: 0, y: 0 });
+      }
+    ];
+
+    testCases.forEach(testCase => testCase());
   });
 });
